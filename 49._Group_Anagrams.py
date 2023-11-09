8b54561b0e0ff915a1669245ce8ad1c5a9353594
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2023-11-09 08:11:23 +0530

    Add files via upload

---
+++ 49._Group_Anagrams.py
@@ -0,0 +1,29 @@
+# Visit the profile for more solutions with minimum complexity
+#https://leetcode.com/sonukkushwaha0801/
+# One way:
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
+    
+#Another way:
+class Solution:
+    def groupAnagrams(self, strs):
+        strs_table = {}
+
+        for string in strs:
+            sorted_string = ''.join(sorted(string))
+
+            if sorted_string not in strs_table:
+                strs_table[sorted_string] = []
+
+            strs_table[sorted_string].append(string)
+
+        return list(strs_table.values())
