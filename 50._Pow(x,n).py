b651cb7800c94df204c0e1a637e6d2445bb5b6fd
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2024-02-11 19:24:22 +0530

    Add files via upload

---
+++ 50._Pow(x,n).py
@@ -0,0 +1,11 @@
+# Visit the profile for more solutions with minimum complexity
+#https://leetcode.com/sonukkushwaha0801/
+# One way:
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)
+
+# Another Way:
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
